00f49b14fa74468920b1e82d716c814e67827d75
billstam12 <billstam12@gmail.com>
2021-07-06 10:55:41 +0300

    Added blockjoin for token maps

--- src/main/java/gr/athenarc/imsi/visualfacts/queryER/QueryTokenMap.java
+++ src/main/java/gr/athenarc/imsi/visualfacts/queryER/QueryTokenMap.java
@@ -6,10 +6,14 @@ import gr.athenarc.imsi.visualfacts.config.ERConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class QueryTokenMap {
     private static final Logger LOG = LogManager.getLogger(QueryTokenMap.class);
@@ -23,6 +27,16 @@ public class QueryTokenMap {
         this.schema = schema;
     }
 
+    public Map<String, Set<String>> joinTokenMap() {
+		TokenMap tokenMap = new TokenMap(this.schema);
+		Map<String, Set<String>> globalTokenMap = tokenMap.map;
+		Predicate<Map.Entry<String, Set<String>>> predicate = entry -> map.keySet().contains(entry.getKey()); // note this is java.util.function.Predicate
+		Map<String, Set<String>> extendedQueryMap = globalTokenMap.entrySet().stream().filter(predicate)
+		        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+		return extendedQueryMap;
+    }
+    
     public void processQueryObject(String[] object) {
         for (CategoricalColumn categoricalColumn : schema.getCategoricalColumns()) {
             String value = object[categoricalColumn.getIndex()];
