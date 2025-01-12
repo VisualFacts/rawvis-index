37faee13cd05aca81d72b8a174ed019eec5e23de
Stavros Maroulis <stavrosmrl@gmail.com>
2025-01-12 16:39:42 +0200

    Fix bugs on approximate valinor

--- src/main/java/gr/athenarc/imsi/visualfacts/ApproximateValinor.java
+++ src/main/java/gr/athenarc/imsi/visualfacts/ApproximateValinor.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Stack;
 import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
@@ -40,8 +39,6 @@ public class ApproximateValinor {
     private Grid grid;
 
     private Schema schema;
-
-    private String initMode;
 
     private String sort = "asc";
 
@@ -234,14 +231,14 @@ public class ApproximateValinor {
             queryNode.maxErrorBound = (queryNode.maxSum - queryNode.minSum) / (queryNode.minSum + queryNode.maxSum);
         }
 
-
         // Sort partially contained nodes by their respective max error bound
         partiallyContainedNodesWithStats.sort(Comparator.comparingDouble(QueryNode::getMaxErrorBound));
 
         int currentIndex = 0;
         double maxErrorBound = calculateMaxErrorBound(partiallyContainedNodesWithStats, currentIndex, queryResults);
 
-        // Process partially contained tiles and read from the file until the error bound is acceptable.
+        // Process partially contained tiles and read from the file until the error
+        // bound is acceptable.
         while (currentIndex < partiallyContainedNodesWithStats.size() && maxErrorBound > errorBound) {
             QueryNode queryNode = partiallyContainedNodesWithStats.get(currentIndex);
             pointIterator = new KWayMergePointIterator(Arrays.asList(new NodePointsIterator(queryNode)));
@@ -257,11 +254,12 @@ public class ApproximateValinor {
             }
         }
 
-        // For the remaining partially contained tiles, we approximate their values using the mean value for all the objects in each tile
+        // For the remaining partially contained tiles, we approximate their values
+        // using the mean value for all the objects in each tile
         while (currentIndex < partiallyContainedNodesWithStats.size()) {
             QueryNode queryNode = partiallyContainedNodesWithStats.get(currentIndex);
             NodePointsIterator nodePointsIterator = new NodePointsIterator(queryNode);
-            
+
             while (nodePointsIterator.hasNext()) {
                 nodePointsIterator.next();
                 queryResults.adjustStats(null, (float) queryNode.getNode().getStats().xStats().mean(), 0f);
@@ -310,7 +308,7 @@ public class ApproximateValinor {
                 randomAccessReader.seek(point.getFileOffset());
                 line = randomAccessReader.readLine();
                 Float measureValue0 = null;
-                Float measureValue1 = null;
+                Float measureValue1 = 0f;
                 if (line != null) {
                     row = parser.parseLine(line);
                     if (row != null) {
@@ -321,28 +319,10 @@ public class ApproximateValinor {
 
                         QueryNode queryNode = pointIterator.getCurrentQueryNode();
                         TreeNode node = queryNode.getNode();
-
                         if (queryNode.isFullyContained()) {
-                            // we expand the node with unknown attrs
-                            if (!initMode.equals("valinor") && queryNode.getUnknownCatAttrs() != null
-                                    && !queryNode.getUnknownCatAttrs().isEmpty()) {
-                                for (CategoricalColumn unknownAttr : queryNode.getUnknownCatAttrs()) {
-                                    node = node.getOrAddChild(unknownAttr.getValueKey(row[unknownAttr.getIndex()]));
-                                }
-                                node.addPoint(point);
-                                if (measureValue0 != null && measureValue1 != null) {
-                                    node.adjustStats(measureValue0, measureValue1);
-                                }
-                            } else if (queryNode.getUnknownCatAttrs() == null
-                                    || queryNode.getUnknownCatAttrs().isEmpty()) {
-                                if (measureValue0 != null && measureValue1 != null) {
-                                    queryNode.getNode().adjustStats(measureValue0, measureValue1);
-                                }
+                            if (measureValue0 != null) {
+                                queryNode.getNode().adjustStats(measureValue0, measureValue1);
                             }
-                        }
-                        if (checkUnknownAttrs(query, row, queryNode.getUnknownCatAttrs()) && measureValue0 != null
-                                && measureValue1 != null) {
-                            queryResults.adjustStats(null, measureValue0, measureValue1);
                         }
                     }
                 }
@@ -351,18 +331,6 @@ public class ApproximateValinor {
             }
         }
         return ioCount;
-    }
-
-    private boolean checkUnknownAttrs(Query query, String[] row, List<CategoricalColumn> unknownCatAttrs) {
-        boolean check = true;
-        for (CategoricalColumn categoricalColumn : unknownCatAttrs) {
-            String filterValue = query.getCategoricalFilters().get(categoricalColumn.getIndex());
-            if (filterValue != null) {
-                String rowValue = row[categoricalColumn.getIndex()];
-                check = check && rowValue != null && rowValue.equals(filterValue);
-            }
-        }
-        return check;
     }
 
     private ContainmentExaminer getContainmentExaminer(Tile tile, Rectangle query) {
