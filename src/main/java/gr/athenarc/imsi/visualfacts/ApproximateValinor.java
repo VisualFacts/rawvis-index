5248aa11d3024d00dda264c256ba436f4236cfe7
Stavros Maroulis <stavrosmrl@gmail.com>
2025-02-17 12:58:51 +0200

    sdgsdfg

--- src/main/java/gr/athenarc/imsi/visualfacts/ApproximateValinor.java
+++ src/main/java/gr/athenarc/imsi/visualfacts/ApproximateValinor.java
@@ -143,7 +143,7 @@ public class ApproximateValinor {
                 if (measureCol0 != null) {
                     Double value0 = Double.parseDouble(row[measureCol0]);
                     Double value1 = 0d;
-                    // node.adjustStats(value0, value1);
+                    node.adjustStats(value0, value1);
                 }
                 if (++objectsIndexed % 1000000 == 0) {
                     LOG.debug("Indexing object " + objectsIndexed);
@@ -292,7 +292,7 @@ public class ApproximateValinor {
         } while (maxErrorBound > errorThreshold);
 
         // Iterate over fully contained query nodes without stats and set their TreeNode's sampled tracker for using in future queries. Their stats have been updated in the readFromFile method
-        // fullyContainedNodesWithoutStats.forEach(queryNode -> queryNode.getNode().setSampledTracker(queryNode.getSampledTracker()));
+        fullyContainedNodesWithoutStats.forEach(queryNode -> queryNode.getNode().setSampledTracker(queryNode.getSampledTracker()));
 
         queryResults.setTileCount(leafTiles.size());
         queryResults.setFullyContainedTileCount(fullyContainedNodesWithStats.size());
