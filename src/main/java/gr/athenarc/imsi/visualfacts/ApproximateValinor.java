b2c9f30c4e80ac7e18c7eb037e9213ec389cde31
Stavros Maroulis <stavrosmrl@gmail.com>
2025-02-23 21:27:05 +0200

    xcbgfb

--- src/main/java/gr/athenarc/imsi/visualfacts/ApproximateValinor.java
+++ src/main/java/gr/athenarc/imsi/visualfacts/ApproximateValinor.java
@@ -292,9 +292,9 @@ public class ApproximateValinor {
         } while (maxErrorBound > errorThreshold);
 
         // Iterate over fully contained query nodes without stats and set their TreeNode's sampled tracker for using in future queries. Their stats have been updated in the readFromFile method
-        fullyContainedNodesWithoutStats.forEach(queryNode -> {
-            queryNode.getNode().setSampledTracker(queryNode.getSampledTracker());
-        });
+        // fullyContainedNodesWithoutStats.forEach(queryNode -> {
+        //     queryNode.getNode().setSampledTracker(queryNode.getSampledTracker());
+        // });
 
         queryResults.setTileCount(leafTiles.size());
         queryResults.setFullyContainedTileCount(fullyContainedNodesWithStats.size());
