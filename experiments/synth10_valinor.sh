ea245aa9b3b1adc9d91495c0ebd8eee5cb4f8648
Stavros Maroulis <stavmars@mourgana.imsi.athenarc.gr>
2026-01-15 08:20:48 +0000

    Refactor synth experiment scripts to vary number of measures

--- experiments/synth10_valinor.sh
+++ experiments/synth10_valinor.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+LIBPATH="$SCRIPT_DIR/../native/build"
+
 # Create the directory if it doesn't exist
-mkdir -p experiments/synth10_only_sampling/different_errors
+mkdir -p experiments/results/synth10/pan_zoom_scenario/
 
 # Define the error bounds
-error_bounds=(0 0.2 0.1 0.05 0.02 0.01)
+error_bounds=(0 0.01 0.02 0.05 0.1 0.2)
+
+# Define sets of measure columns (adjust as needed)
+measure_cols_list=("2" "2,3" "2,3,4,5,6,7" "2,3,4,5,6,7,8,9")
+#  measure_cols_list=("2,3,4,5")
 
 # Number of times to run each experiment
-num_runs=1
+num_runs=3
 
-# Iterate over each error bound and run the experiment multiple times
-for error_bound in "${error_bounds[@]}"
+for measure_cols in "${measure_cols_list[@]}"
 do
-    for run in $(seq 1 $num_runs)
+    num_cols=$(echo $measure_cols | awk -F',' '{print NF}')
+    for error_bound in "${error_bounds[@]}"
     do
-        echo "Running experiment with errorBound $error_bound, run $run..."
-        java -Xmx16G -jar target/experiments.jar -c \
-        timeApproximateQueries -csv /data-nonraid/maroulis/data/data_10_cols.csv -bounds 0:1000,0:1000 \
-        -rect 544:574,323:353 -xCol 0 -yCol 1 -measureCols 9 \
-        -initMode valinor \
-        -objCount 100000000 -seqCount 100 -minShift 10 \
-        -maxShift 20 -out experiments/synth10_new/different_errors/results_${error_bound}_run${run}.csv \
-        -errorBound $error_bound -run $run
+        for run in $(seq 1 $num_runs)
+        do
+            echo "Running experiment with $num_cols measureCols ($measure_cols), errorBound $error_bound, run $run..."
+            java -Xmx16G -Djava.library.path="$LIBPATH" -jar target/experiments.jar -c \
+            timeApproximateQueries -csv /data-nonraid/maroulis/data/data_10_cols.csv -bounds 0:1000,0:1000 \
+            -rect 544:574,323:353 -xCol 0 -yCol 1 -measureCols $measure_cols \
+            -initMode valinor \
+            -objCount 100000000 -seqCount 100 -minShift 10 \
+            -maxShift 20 -zoomFactor 1.5 -out experiments/results/synth10/pan_zoom_scenario/results_mcols${num_cols}_error${error_bound}_run${run}.csv \
+            -errorBound $error_bound -run $run
+        done
     done
 done
 
